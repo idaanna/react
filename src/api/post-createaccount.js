a94fde0bca7b6671161e25532e7c361b0e260670
Ida Eriksson <annaida.eriksson@gmail.com>
2024-02-23 13:34:06 +0800

    Create account, not working

---
+++ src/api/post-createaccount.js
@@ -0,0 +1,29 @@
+async function postCreateAccount(username, password) {
+    const url = `${import.meta.env.VITE_API_URL}/createaccount/`;
+    const response = await fetch(url, {
+        method: "POST", 
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password,
+        }),
+    });
+    
+    if (!response.ok) {
+        const fallbackError = `Error trying to create account`;
+
+        // const data = await response.json().catch(() => {
+        // throw new Error(fallbackError);
+        // });
+
+        // const errorMessage = data?.detail ?? fallbackError;
+        // throw new Error(errorMessage);
+    }
+
+    return await response.json();
+}
+
+export default postCreateAccount;
+        
--- src/components/CreateAccountForm.jsx
+++ src/components/CreateAccountForm.jsx
@@ -1,15 +1,62 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+
+import postCreateAccount from "../api/post-createaccount";
+
 function CreateAccountForm() {
+    const navigate = useNavigate();
+
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+        });
+
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
+        }));
+    };
+
+     const handleSubmit = (event) => {
+         event.preventDefault();
+         if (credentials.username && credentials.password) {
+            postCreateAccount(
+                credentials.username,
+                credentials.password
+            ).then((response) => {
+                console.log(response);
+                navigate("/");
+            });
+         }
+    };
+
     return (
         <form>
             <div>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" placeholder="Enter username" />
+                <label htmlFor="username"> Create Username: </label>
+                <input 
+                    type="text"
+                    id="username"
+                    placeholder="Create username"
+                    onChange={handleChange}
+                />
+                    
             </div>
             <div>
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" placeholder="Password" />
+                <label htmlFor="password">Create Password: </label>
+                <input 
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    />
+                    
             </div>
-        <button type="submit"> Create Account </button>
+            <button type="submit" onClick={handleSubmit}>
+                Create Account 
+            </button>
         </form>
      );
 }
