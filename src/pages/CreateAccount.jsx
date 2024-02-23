f4a205f5ff6bd7d624564aa8df14c32e4b651887
Ida Eriksson <annaida.eriksson@gmail.com>
2024-02-23 12:23:29 +0800

    Initial CSS set up

--- src/pages/CreateAccount.jsx
+++
@@ -1,5 +0,0 @@
-function CreateAccountPage() {
-    return <h1>This is the Create Account page</h1>;
-    }
-    export default CreateAccountPage;
-    
---
+++ src/pages/CreateAccountPage.jsx
@@ -0,0 +1,7 @@
+import CreateAccountForm from "../components/CreateAccountForm";
+
+function CreateAccountPage() {
+    return < CreateAccountForm />;
+    }
+    export default CreateAccountPage;
+    
