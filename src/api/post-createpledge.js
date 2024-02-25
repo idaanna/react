ce1740e891a07858ecdc4443f7284771bfde1c8a
Ida Eriksson <annaida.eriksson@gmail.com>
2024-02-25 14:31:07 +0800

    updated to include pledgeData. for ease of use

--- src/api/post-createpledge.js
+++ src/api/post-createpledge.js
@@ -1,14 +1,15 @@
-async function postPledge(amount, comment) {
+async function postPledge(pledgeData) {
     const url = `${import.meta.env.VITE_API_URL}/pledges/`;
     const response = await fetch(url, {
-        method: "POST", // We need to tell the server that we are sending JSON data
-    // so we set the Content-Type header to application/json
+        method: "POST", 
         headers: {
         "Content-Type": "application/json",
         },
         body: JSON.stringify({
-        "amount": amount,
-        "comment": comment,
+        "amount": pledgeData.amount,
+        "comment": pledgeData.comment,
+        "is_anonymous": pledgeData.isAnonymous,
+        "project": pledgeData.project,
         }),
     });
     
