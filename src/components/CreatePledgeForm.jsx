3b8595b948a1f9708c03211d8685237839f96d97
Ida Eriksson <annaida.eriksson@gmail.com>
2024-03-01 09:11:59 +0800

    commented out code to trouble shoot

--- src/components/CreatePledgeForm.jsx
+++ src/components/CreatePledgeForm.jsx
@@ -1,66 +1,83 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import { useParams } from "react-router-dom";
 
 import postPledge from "../api/post-createpledge";
 
 
 function CreatePledgeForm() {
     const navigate = useNavigate();
+    const { id } = useParams();
 
     const [pledge, setPledge] = useState({
         amount: null,
         comment: "",
-        is_open: true,
-        date_created: new Date().toISOString(),
+        is_Anonymous:false,
+        // is_open: true,
+        // date_created: new Date().toISOString(),
         project: "",
-        supporter:"",
-        isAnonymous:false,
+        // supporter:"",       
     });
     const handleChange = (event) => {
         const { id, value } = event.target;
-        setPledge((prevPledgeData) => ({
-            ...prevPledgeData,
-            [id]: value,
+        setPledge((prevPledge) => ({
+            ...prevPledge,
+            [id]: id,
         }));
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        if (pledgeData.amount && pledgeData.comment) {
-            // postPledge(
-            //     pledgeData
-            // ).then((newpledge) => {
-            //     // Handle the response if needed
-            //     console.log("Pledge created successfully:", newpledge);
-            //     // Redirect the user to the project detail page or any desired location
-            //     navigate(`/pledges`);
-            // })
-            //     .catch((error) => {
-            //         // Handle errors, e.g., show an error message to the user
-            //         console.error("Error creating pledge:", error);
-            //     });
-        }    
-    }
+
+        const authToken = window.localStorage.getItem("token");
+        if (authToken) {
+            postPledge(
+                pledge.amount,
+                pledge.comment,
+                // pledge.is_Anonymous,
+                authToken
+            ).then((response) => {
+                console.log(response);
+                navigate("/");
+            });
+        }
+    };
+    //     if (pledge.amount && pledge.comment) {
+    //         console.log(pledge)
+    //         postPledge(
+    //             pledge
+    //         ).then((newpledge) => {
+    //             // Handle the response if needed
+    //             console.log("Pledge created successfully:", newpledge);
+    //             // Redirect the user to the project detail page or any desired location
+    //             navigate(`/pledges`);
+    //         })
+    //             .catch((error) => {
+    //                 // Handle errors, e.g., show an error message to the user
+    //                 console.error("Error creating pledge:", error);
+    //             });
+    //     }    
+    // }
             return (
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                     <label htmlFor="amount"> Insert pledge amount: </label>
-                        <input 
-                            type="number"
-                            id="amount"
-                            placeholder="Insert pledge amount"
-                            onChange={handleChange}
-                        />
+                    <input 
+                        type="number"
+                        id="amount"
+                        placeholder="Insert pledge amount"
+                        onChange={handleChange}
+                    />
                     </div>
 
                     <div>
                     <label htmlFor="comment"> Insert pledge comment: </label>
-                        <input 
-                            type="text"
-                            id="comment"
-                            placeholder="Insert pledge comment"
-                            onChange={handleChange}
-                        />
+                    <input 
+                        type="text"
+                        id="comment"
+                        placeholder="Insert pledge comment"
+                        onChange={handleChange}
+                    />
                     </div>
 
                     <div>
@@ -68,12 +85,12 @@ function CreatePledgeForm() {
                     <input
                         type="checkbox"
                         id="isAnonymous"
-                        value={pledge.isAnonymous}
+                        value={pledge.is_Anonymous}
                         onChange={handleChange}
                     />
                     </div>
 
-                    <button type="submit" onClick={handleSubmit}>
+                    <button type="submit">
                         Create Pledge 
                     </button>
                 </form>
